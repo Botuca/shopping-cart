28df3e7334c69968b42a2fb65bcc51463fae1c95
Botuca <botucacontato@gmail.com>
2023-06-02 23:43:12 -0300

    atualizado package-lock

---
+++ components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="flex fixed items-center justify-center h-16 bg-gray-300 w-full border-b border-gray-400">
+    <h1 class="font-bold text-xl">
+      SHOPPING CART
+    </h1>
+    <i class="fa-solid fa-cart-shopping absolute right-5 text-2xl"></i>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBase',
+}
+</script>
