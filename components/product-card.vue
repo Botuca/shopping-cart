d854013ccfec04d447fc2b3a77b3e31fdc736850
Botuca <botucacontato@gmail.com>
2023-06-14 23:16:59 -0300

    add div de cart

--- components/product-card.vue
+++ components/product-card.vue
@@ -1,16 +1,40 @@
 <template>
   <section>
-    <div
-      class="h-96 w-64 border border-gray-300"
-    >
-      <img :src="product.image" class="object-contain h-48 w-48 m-auto pt-2 cursor-pointer" @click="$emit('onClickProduct', product)" />
+    <div class="h-96 w-64 border border-gray-300">
+      <img
+        :src="product.image"
+        class="object-contain h-48 w-48 m-auto pt-2 cursor-pointer"
+        @click="$emit('onClickProduct', product)"
+      />
       <div class="p-2">
-        <span class="pt-4 text-ellipsis h-16 font-bold">{{ product.title }}</span>
+        <span class="pt-4 text-ellipsis h-16 font-bold">
+          {{ product.title }}
+        </span>
         <div class="flex flex-col">
-          <span class="pt-2 text-xs text-ellipsis"><b>Description: </b>{{ product.description }}</span>
-          <span class="pt-1 text-xs"><b>Category: </b>{{ product.category }}</span>
-          <span class="pt-1 text-xs"><b>Rating: </b>{{ product.rating.rate }} ({{ product.rating.count }})</span>
-          <span class="text-red-500 text-xl font-bold pt-2">${{ product.price }}</span>
+          <span class="pt-2 text-xs text-ellipsis">
+            <b> Description: </b>
+            {{ product.description }}
+          </span>
+          <span class="pt-1 text-xs">
+            <b> Category: </b>
+            {{ product.category }}
+          </span>
+          <span class="pt-1 text-xs">
+            <b> Rating: </b>
+            {{ product.rating.rate }} ({{ product.rating.count }})
+          </span>
+          <div class="pt-2 flex justify-between items-center">
+            <span class="text-red-500 text-xl font-bold">
+              ${{ product.price }}
+            </span>
+            <button
+              class="text-xs bg-yellow-400 rounded px-2 py-1 font-bold hover:bg-yellow-500"
+              @click="onClickIcon"
+            >
+              Add to
+              <i class="fa-solid fa-cart-shopping text-xs" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -24,17 +48,17 @@
       product: {
         type: Object,
         required: true,
-      }
-    }
+      },
+    },
   }
 </script>
 
 <style scoped>
-.text-ellipsis {
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
+  .text-ellipsis {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 </style>
